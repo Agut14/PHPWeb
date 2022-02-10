9a057881fdad307b09f94c7503047675e5410a22
Carlos Agut <carlosagutvives@gmail.com>
2022-02-10 09:31:33 +0100

    Ficheros y Directorios

---
+++ Curso PHP/Directorios/index.php
@@ -0,0 +1,10 @@
+<?php 
+//mkdir para crear un directorio
+//is_dir para comprobar si existe el directorio
+//borrar carpeta con rmdir
+//opendir para abrir el directorio
+//leer el directorio con readdir
+
+
+
+?>
---
+++ Curso PHP/Ficheros/index.php
@@ -0,0 +1,23 @@
+<?php
+
+//Se puede copiar el archivo a otro con copy(), o renombrarlo con rename() o eliminar con unlink()
+//comprobar si un archivo existe con file_existe()
+
+//Abrir archivo
+$archivo = fopen("ficheroTexto.txt", "a+");
+
+//Leer archivo
+while(!feof($archivo)){
+    $contenido = fgets($archivo);
+    echo $contenido;
+}
+$contenido = fgets($archivo);
+echo $contenido;
+
+//Escribir en archivo
+fwrite($archivo, " Texto desde PHP");
+
+//Cerrar archivo
+fclose($archivo);
+
+?>
