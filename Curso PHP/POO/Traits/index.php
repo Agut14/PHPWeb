84cce6cdccfc621bedaece5c51c8880d334cf49f
Carlos Agut <carlosagutvives@gmail.com>
2022-02-21 20:51:28 +0100

    traits

---
+++ Curso PHP/POO/Traits/index.php
@@ -0,0 +1,35 @@
+<?php
+
+trait Utilidades{
+    public function mostrarNombre(){
+        echo "<h1>El nombre es ".$this->nombre."</h1>";
+    }
+}
+
+class Vehiculo{
+    public $nombre = "Audi";
+    public $color;
+
+    use Utilidades;
+
+}
+
+class Persona{
+    public $nombre = "Carlos";
+    public $edad;
+
+    use Utilidades;
+}
+
+class Videojuego{
+    public $nombre;
+    public $tipo;
+}
+
+$audi = new Vehiculo();
+$persona2 = new Persona();
+//$audi->mostrarNombre();
+$persona2->mostrarNombre();
+
+
+?>
